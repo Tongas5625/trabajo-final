7a1704c128965fa2e45058a3a174be11aa09b438
Gaston <gaston590166@gmail.com>
2022-12-19 10:48:14 -0300

    actualizado

--- supermarket.py
+++ supermarket.py
@@ -8,25 +8,25 @@ class Product:
     
     def __init__(self, window):
         self.wind = window
-        self.wind.title('Products Application')
+        self.wind.title('SuperMarket')
 
         # Creating a Frame Container
-        Frame = LabelFrame(self.wind, text='Register a new product')
+        Frame = LabelFrame(self.wind, text='Registar un Producto')
         Frame.grid(row=0, column=0, columnspan=3, pady=20)
 
         # Name Input
-        Label(Frame, text='Name').grid(row=1, column=0)
+        Label(Frame, text='Nombre').grid(row=1, column=0)
         self.name = Entry(Frame)
         self.name.focus()
         self.name.grid(row=1, column=1)
 
         # Price Input
-        Label(Frame, text='Price: ').grid(row=2, column=0)
+        Label(Frame, text='Precio: ').grid(row=2, column=0)
         self.price = Entry(Frame)
         self.price.grid(row=2, column=1)
 
         # Button Add Product
-        ttk.Button(Frame, text='Save product', command=self.add_product).grid(row=3, columnspan=2, sticky= W + E)
+        ttk.Button(Frame, text='Guardar Producto', command=self.add_product).grid(row=3, columnspan=2, sticky= W + E)
 
         # Output Messages
         self.message = Label(text='', fg='red')
@@ -35,12 +35,12 @@ class Product:
         # Table
         self.tree = ttk.Treeview(height=10, columns=2)
         self.tree.grid(row=4, column=0, columnspan=2)
-        self.tree.heading('#0', text='Name', anchor=CENTER)
-        self.tree.heading('#1', text='Price', anchor=CENTER)
+        self.tree.heading('#0', text='Nombre', anchor=CENTER)
+        self.tree.heading('#1', text='Precio', anchor=CENTER)
         
         # Buttons
-        ttk.Button(text='DELETE', command=self.delete_product).grid(row=5, column=0, sticky=W+E)
-        ttk.Button(text='EDIT', command=self.edit_product).grid(row=5, column=1, sticky=W+E)
+        ttk.Button(text='ELIMINAR', command=self.delete_product).grid(row=5, column=0, sticky=W+E)
+        ttk.Button(text='EDITAR', command=self.edit_product).grid(row=5, column=1, sticky=W+E)
         
         # Filling the Row
         self.get_products()
@@ -72,11 +72,11 @@ class Product:
             query = 'INSERT INTO product VALUES(NULL, ?, ?)'
             parameters = (self.name.get(), self.price.get())
             self.run_query(query, parameters)
-            self.message['text'] = 'Product {} added Successfully'.format(self.name.get())
+            self.message['text'] = 'Producto {} agregado Satisfactoriamente'.format(self.name.get())
             self.name.delete(0, END)
             self.price.delete(0, END)
         else:
-            self.message['text'] = 'Name and Price are Required'
+            self.message['text'] = 'Se requiere el nombre y precio'
         self.get_products()
         
     def delete_product(self):
@@ -84,13 +84,13 @@ class Product:
         try:
             self.tree.item(self.tree.selection())['text'][0]
         except IndexError as e:
-            self.message['text'] = 'Please select a record'
+            self.message['text'] = 'Por favor, seleccion un registro'
             return
         self.message['text'] = ''
         name = self.tree.item(self.tree.selection())['text']
         query = 'DELETE FROM product WHERE name = ?'
         self.run_query(query, (name, ))
-        self.message['text'] = 'Record {} deleted Successfully'.format(name)
+        self.message['text'] = 'Registro {} borrado Satisfactoriamente'.format(name)
         self.get_products()
         
     def edit_product(self):
@@ -98,40 +98,39 @@ class Product:
         try:
             self.tree.item(self.tree.selection())['text'][0]
         except IndexError as e:
-            self.message['text'] = 'Please select a record'
-            return
+            self.message['text'] = 'Por favor, seleccion un registro'
         name = self.tree.item(self.tree.selection())['text']
         old_price = self.tree.item(self.tree.selection())['values'][0]
         
         # Generate a other wind
         self.edit_wind = Toplevel()
-        self.edit_wind.title = 'Edit Product'
+        self.edit_wind.title = 'Editar Producto'
         
         # Old Name
-        Label(self.edit_wind, text='Old Name: ').grid(row=0, column=1)
+        Label(self.edit_wind, text='Nombre anterior: ').grid(row=0, column=1)
         Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value= name), state='readonly').grid(row=0,column=2)
         # New Name
-        Label(self.edit_wind, text='New Name: ').grid(row=1, column=1)
+        Label(self.edit_wind, text='Nombre nuevo: ').grid(row=1, column=1)
         new_name = Entry(self.edit_wind)
         new_name.grid(row=1, column=2)
         
         # Old Price
-        Label(self.edit_wind, text='Old Price: ').grid(row=2, column=1)
+        Label(self.edit_wind, text='Precio viejo: ').grid(row=2, column=1)
         Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value= old_price), state='readonly').grid(row=2,column=2)
         # New Price
-        Label(self.edit_wind, text='New Price: ').grid(row=3, column=1)
+        Label(self.edit_wind, text='Precio nuevo: ').grid(row=3, column=1)
         new_price = Entry(self.edit_wind)
         new_price.grid(row=3, column=2)
         
         
-        Button(self.edit_wind, text='Update',  command = lambda: self.edit_records(new_name.get(), name, new_price.get(), old_price)).grid(row=4, column=2, sticky= W)
+        Button(self.edit_wind, text='Actualizar',  command = lambda: self.edit_records(new_name.get(), name, new_price.get(), old_price)).grid(row=4, column=2, sticky= W)
         
     def edit_records(self, new_name, name, new_price, old_price):
         query = 'UPDATE product SET name = ?, price = ? WHERE name = ? AND price = ?'
         parameters = (new_name, new_price, name, old_price)
         self.run_query(query, parameters)
         self.edit_wind.destroy()
-        self.message['text'] = 'Record {} update Successfully'.format(name)
+        self.message['text'] = 'Registro {} actualizado Satisfactoriamente'.format(name)
         self.get_products()
         
 if __name__ == '__main__':
